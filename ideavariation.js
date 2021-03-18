f6d371196aadd685d90611e179e7c3f80f0bb36f
corey <corey.grant.durham@hotmail.co.uk>
2021-03-18 22:11:44 +0000

    lots more work and icons

--- ideavariation.js
+++ ideavariation.js
@@ -1,9 +1,6 @@
-(function(){
-   
-    var data = window.getData();
-    var searchBonus = data.ideaBonuses.reduce((prev, cur) =>
-        Object.assign({[cur]: false}, prev),
-        {});
+(async function(){
+    
+    var data = await window.getData();
     var searchIdeas = data.ideas.map(x => x.name).reduce((prev, cur) =>
         Object.assign({[cur]: false}, prev),
         {});
@@ -14,164 +11,88 @@
             ideas: data.ideas,
             policies: data.policies,
             tab: 'idea-groups',
-            searchByBonusTab: 'ideas',
-            bonuses: data.ideaBonuses.sort(),
+            exclusiveCategories: ["General", "Imperial", "Ship", "Army", "Government", "Damage", "Centralism", "Religious"],
+            ideaBonuses: data.ideaBonuses.sort(),//.concat(data.policyBonuses)
+            //     .filter((value, i, a) => a.indexOf(value) === i)
+            //     .sort(),
+            policyBonuses: data.policyBonuses.sort(),
             search: {
-                searchBonus: searchBonus,
-                policyBonus: Object.assign({}, searchBonus),
-                ideas: Object.assign({}, searchIdeas),
+                ideaBonus: '',
+                ideaExclusiveCategory: '',
+                policyBonus: '',
+                ideas: searchIdeas,
                 ideaMonarch: '',
                 policyMonarch: '',
             }
         },
         methods:{
-            getBonusDisplay: function(b){
-                var parts = b.split(' ');
-                var output = '';
-                for(var i = 0; i < parts.length; i++){
-                    if(i == 0){
-                        output += (parts[i][0].toUpperCase() + parts[i].substring(1));
-                    } else {
-                        output += parts[i];
-                    }
-                    if(i < (parts.length - 1)){
-                        output += ' ';
-                    }
-                }
-                return output;
-            },
-            getBonusAcronym: function(b){
-                return b.split(' ')
-                    .map(function(x){ return x[0].toUpperCase() })
-                    .join('');
-            },
-            toggleBonusSearch(bonus){
-                this.search.searchBonus[bonus] = !this.search.searchBonus[bonus];
-            },
-            toggleSearchByIdea(name){
-                if(this.search.ideas[name]){
-                    this.search.ideas[name] = false;
-                } else{
-                    this.search.ideas = Object.assign({}, searchIdeas);
-                    this.search.ideas[name] = true;
-                }
-            },
-            bonusIsSearched(bonus){
-                var searchedBonuses = this.bonuses.filter(x => this.search.searchBonus[x]);
-                return Object.getOwnPropertyNames(bonus).some(x => searchedBonuses.indexOf(x) > -1);
-            },
             getAllow(allow){
                 var key = 'full_idea_group';
                 var keys = Object.getOwnPropertyNames(allow).filter(x => x.startsWith(key)).map(x => allow[x]);
-                return keys.join("/");
+                var name = keys.join("/");
+                return name;
+            },
+            getAge(allow){
+                return allow.current_age ? `[${allow.current_age}]` : '';
             },
             getImageLink(item){
                 return window.imageMap(item);
             },
-            hasImageLink(item){
-                return window.imageMapExists(item);
-            },
             displayBonusVal(bonusName, bonusVal){
-                // Has decimal place but shouldnt be percentaged
-                if(["Interest", "Yearly inflation reduction"].indexOf(bonusName) !== -1){
-                    return parseFloat(bonusVal).toFixed(2);
-                }
-                if(["Yearly absolutism"].indexOf(bonusName) !== -1){
-                    return bonusVal;
-                }
-                // Has decimal place or otherwise SHOULD be percentaged
-                if(bonusVal.indexOf('.') > -1 || [].indexOf(bonusName) !== -1){
-                    var valNum = parseFloat(bonusVal);
-                    var percentNum = valNum * 100;
-                    if(Number.isInteger(percentNum)){
-                        return `${(valNum * 100).toFixed(0)}%`;
-                    }
-                    return `${(valNum * 100).toFixed(1)}%`;
-                }
-                return bonusVal;
+                if(!bonusName || !bonusVal){return '';}
+                return window.formatDisplay(bonusName, bonusVal);
+            },
+            toggleIdeaMonarch(val){
+                this.search.ideaMonarch = this.search.ideaMonarch === val
+                    ? ''
+                    : val;
+            },
+            togglePolicyMonarch(val){
+                this.search.policyMonarch = this.search.policyMonarch === val
+                    ? ''
+                    : val;
             }
             
         },
         computed:{
             filteredIdeas: function(){
-                var activeBonusFilters = this.bonuses
-                    .filter(x => this.search.searchBonus[x]);
+                var activeBonusFilter = this.search.ideaBonus;
                 var activeMonarchFilters = this.search
                     .ideaMonarch;
-                if(!activeMonarchFilters && !activeBonusFilters){
-                    return [];
-                }
+                    var activeIdeaExclusiveCategory = this.search.ideaExclusiveCategory;
                 var ideas = this.ideas;
-                if(activeBonusFilters.length){
-                    ideas = activeBonusFilters.map(s =>
-                        ideas.filter(x => x.bonuses.some(x => x.bonus[s] !== undefined)))
-                        .flat().filter((value, i, a) => a.indexOf(value) === i);
+                if(activeBonusFilter.length){
+                    ideas = 
+                        ideas.filter(x => x.bonuses.some(x => x.bonus[activeBonusFilter] !== undefined));
+                        
+                }
+                if(activeIdeaExclusiveCategory.length){
+                    ideas = 
+                        ideas.filter(x => x.exclusiveCategory === activeIdeaExclusiveCategory);
+                        
                 }
                 if(activeMonarchFilters){
                     ideas = ideas.filter(x => x.category == activeMonarchFilters);
                 }
-                return ideas;
+                return ideas.sort((a, b) => a.name > b.name );
             },
             filteredPolicies: function(){
-                var activeBonusFilters = this.bonuses
-                    .filter(x => this.search.policyBonus[x]);
+                var activeBonusFilter = this.search.policyBonus;
                 var activeMonarchFilters = this.search
                     .policyMonarch;
                 var activeIdeaGroups = this.ideas.map(x => x.name)
                     .filter(x => this.search.ideas[x]);
-                if(!activeBonusFilters && !activeMonarchFilters && !activeIdeaGroups){
-                    return [];
-                }
                 var policies = this.policies;
-                if(activeBonusFilters.length){
-                    policies = activeBonusFilters.map(s =>
-                        policies.filter(x => x.bonuses.some(x => x.bonus[s] !== undefined)))
-                        .flat().filter((value, i, a) => a.indexOf(value) === i);
+                if(activeBonusFilter.length){
+                    policies = 
+                        policies.filter(x => x.bonuses.some(x => x.name == activeBonusFilter));
+                      
                 }
                 if(activeMonarchFilters){
-                    policies = policies.filter(x => x.category == activeMonarchFilters);
+                    policies = policies.filter(x => x.monarchPower == activeMonarchFilters);
                 }
                 return policies;
             },
-            ideaSearchResults: function(){
-                var activeSearches = this.bonuses
-                    .filter(x => this.search.searchBonus[x]);
-                if(!activeSearches.length > 0){
-                    return [];
-                } else{
-                    return activeSearches.map(s =>
-                        this.ideas.filter(x => x.bonuses.some(x => x.bonus[s] !== undefined)))
-                        .flat().filter((value, i, a) => a.indexOf(value) === i);
-                }
-            },
-            policySearchResults: function(){
-                var activeSearches = this.bonuses
-                    .filter(x => this.search.searchBonus[x]);
-                if(!activeSearches.length > 0){
-                    return [];
-                } else{
-                    var results = activeSearches.map(s =>
-                        this.policies.filter(x => x.bonuses.some(x => x.name == s)))
-                        .flat().filter((value, i, a) => a.indexOf(value) === i);
-                    return results;
-                }
-            },
-            policyByIdeaSearchResults(){
-                var activeSearches = this.ideas.map(x => x.name)
-                    .filter(x => this.search.ideas[x]);
-                if(!activeSearches.length > 0){
-                    return [];
-                } else{
-                    return activeSearches.map(x => 
-                        this.policies.filter(p => {
-                            var allow = p.allow;
-                            var key = 'full_idea_group';
-                            var keys = Object.getOwnPropertyNames(allow).filter(x => x.startsWith(key)).map(x => allow[x]);
-                            return keys.indexOf(x) > -1;
-                        })
-                    ).flat().filter((value, i, a) => a.indexOf(value) === i);
-                }
-            }
         }
     })
 }());
--- imagemappings.js
+++ imagemappings.js
@@ -5,21 +5,58 @@ var monarchPowerMappings = {
 };
 
 var bonusMappings = {
-    "Infantry combat ability": "Infantry_combat_ability.png",
-    "Cavalry combat ability": "Cavalry_combat_ability.png",
-    "Artillery combat ability": "Artillery_combat_ability.png",
-    "Colonial range": "Colonial_range.png",
-    "Colonists": "Colonists.png",
-    "Idea cost": "Idea_cost.png",
-    "May explore": "May_explore.png",
-    "Missionaries": "Missionaries.png",
-    "Missionary strength vs heretics": "Missionary_strength_vs_heretics.png",
-    "Missionary maintenance cost": "Missionary_maintenance_cost.png",
-    "Missionary strength": "Missionary_strength.png",
-    "Monarch admin power": "Monarch_administrative_power.png",
-    "Monarch diplomatic power": "Monarch_diplomatic_power.png",
-    "Monarch military power": "Monarch_military_power.png",
+    "Interest": "Interest_per_annum.png",
+    "Goods Produced": "Goods_produced_modifier.png",
+    "Reduce inflation cost reduction" : "Reduce_inflation_cost.png",
+    "Institution spread from true faith": "Institution_spread_in_true_faith_provinces.png",
+    "Global institution spread": "Institution_spread.png",
+    "Global institution growth": "Institution_growth.png",
+    "Backrow artillery damage": "Artillery_damage_from_back_row.png",
+    "Hostile attrition": "Attrition_for_enemies.png",
+    "Fort maintenance modifier": "Fort_maintenance.png",
+    "Yearly papal influence": "Papal_influence.png",
+    "Church power modifier": "Church_power.png",
+    "Monthly fervor increase": "Monthly_fervor.png",
+    "Tolerance of true faith": "tolerance_of_the_true_faith.png",
+    "Defensiveness": "Fort_defense.png",
+    "Fire damage": "Land_fire_damage.png",
+    "National manpower": "Manpower.png",
+    "Manpower per age": "Manpower.png",
+    "National sailors": "Sailors.png",
+    "Sailors per age": "Sailors.png",
+    "Diplomats": "Diplomat.png",
+    "Reduced stability impact from diplo": "Reduced_stab_impacts.png",
+    "Improve relations modifier": "Improve_relations.png",
+    "Yearly absolutism": "Absolutism.png",
+    "Diplo tech cost modifier": "Diplomatic_technology_cost.png",
+    "Admin tech cost modifier": "Administrative_technology_cost.png",
+    "Military tech cost modifier": "Military_technology_cost.png",
+    "Add CB": "Casus_belli.png",
+    "Relation with heretics": "Opinion_of_heretics.png",
+    "Land attrition": "Attrition.png",
+    "Number of accepted cultures": "max_promoted_cultures.png",
+    "Tolerance of heathens": "tolerance_heathen.png",
+    "Tolerance of heretics": "Tolerance_heretic.png",
+    "Autonomy modifier": "Autonomy.png",
+    "Global supply limit modifier": "Supply_limit.png",
+    "Trade range modifier": "trade_range.png",
+    "Global trade power": "Trade_power.png",
+    "State maintenance modifier": "State_maintenance.png",
+    "Tariffs": "local_tariffs.png",
+    "Reinforce cost modifier": "reinforce_cost.png",
+    "Marine fraction": "marines_force_limit.png",
+    "Siege blockade progress": "ab_siege_blockades.png",
+    "Morale bonus from 5 cultures": "morale_of_armies.png",
+    "Enforce religion cost": "Cost_of_enforcing_religion_through_war.png",
+    "Build cost in subject nation": "construction_cost.png",
+    "Sailor maintenance modifier": "sailor_maintainance.png",
+    "Add naval force limit per age": "naval_force_limit.png",
+    "Extra navy tradition from galleys": "yearly_navy_tradition.png",
+    "Devepment cost for provinces over 25 dev": "Development_cost.png"
+}
 
+function defaultMap(name){
+    return name.split(' ').join('_') + ".png";
 }
 
 window.imageMap = function(thingToMap){
@@ -29,8 +66,5 @@ window.imageMap = function(thingToMap){
     if(bonusMappings[thingToMap]){
         return 'Icons/Bonuses/' + bonusMappings[thingToMap];
     }
-}
-
-window.imageMapExists = function(thingToMap){
-    return monarchPowerMappings[thingToMap] || bonusMappings[thingToMap];
+    return 'Icons/Bonuses/' + defaultMap(thingToMap);
 }
